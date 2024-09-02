b58c8b323308a9561958d8538e39edc0f455b98f
Dan Sweeney <dan.sweeney@greatdetail.com>
2024-09-02 16:36:19 +0100

    put in the shared bits

---
+++ Assets/gameobj1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExperimentShared;
+
+public class gameobj1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("this is project 1 - " + TestClass.TestString());
+    }
+
+}
